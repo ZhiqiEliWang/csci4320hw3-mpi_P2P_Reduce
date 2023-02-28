6ceb542a7ee3d5ae2bfa28c36bcf021976a791c6
Zhiqi(Eli) Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-02-27 20:57:53 -0500

    Update comp_reduction.c

--- comp_reduction.c
+++ comp_reduction.c
@@ -27,7 +27,7 @@ int MPI_P2P_Reduce(long long int* send_data, // each process's partition of task
     // --------------2. Compute pairwise sums between MPI ranks-------------------
     int stride = 1;
     while (stride < comm_size){
-        if ((self_rank / stride) % 2){ // even ranks after stride: receiver
+        if ((self_rank / stride) % 2 == 0){ // even ranks after stride: receiver
             long long int recv_buf;
             MPI_Request recv_req;
             MPI_Status recv_status;
