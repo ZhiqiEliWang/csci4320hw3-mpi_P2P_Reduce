a76dee446d097097308f9906d961c50efbacb4bc
Zhiqi(Eli) Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-02-28 20:12:17 -0500

    Update comp_reduction.c

--- comp_reduction.c
+++ comp_reduction.c
@@ -13,7 +13,7 @@ int MPI_P2P_Reduce(long long int* send_data, // each process's partition of task
     MPI_Comm communicator) // we only have one communicator
     {
 
-    long long int  local_sum = 0;
+    long long int local_sum = 0;
     int self_rank;
     MPI_Comm_rank(communicator, &self_rank); // get current rank number
     int comm_size;
@@ -54,47 +54,10 @@ int MPI_P2P_Reduce(long long int* send_data, // each process's partition of task
     return 0;
 }
 
-void MPI_P2P_Reduce2(long long *sendbuf, void *recvbuf, int count,
-                    MPI_Datatype datatype, int target,
-                    MPI_Comm comm) {
-  int stride = 1;
-  int rank, size;
-  int curRank = MPI_Comm_rank(comm, &rank);
-
-  long long sum = 0; 
-  for (int i = 0; i < count; i++) { 
-    sum += sendbuf[i];
-  }
-  MPI_Barrier(comm);
-  //printf("count: %d sum: %lld\n", count, sum);
-  //printf("%d", MPI_Comm_size(comm, &size));
-  MPI_Comm_size(comm, &size);
-  //printf("size: %d\n", size);
-  //MPI_Comm_size(comm, &size);
-  while (stride < size) {
-    //printf("%d %d\n", size, rank);       
-    if ((rank / stride) % 2) {  // sender
-      MPI_Request req;
-      MPI_Isend(&sum, 1, MPI_LONG_LONG, rank - stride, 0, comm, &req);
-      MPI_Wait(&req, MPI_STATUS_IGNORE);
-    } else {  // receiver
-      long long temp;
-      MPI_Request req;
-      MPI_Irecv(&temp, 1, MPI_LONG_LONG, rank + stride, MPI_ANY_TAG, comm, &req);
-      MPI_Wait(&req, MPI_STATUS_IGNORE);
-      sum += temp;
-      if (sum >= 576460751766552576) 
-          printf("temp: %lld\n", sum);
-    }
-    stride *= 2;  
-    MPI_Barrier(comm);
-  }
-  return;
-}
 
 
 
-int main(int argc, char** argv){
+int main(int argc, char* argv[]){
   // Initialize the MPI environment
     int world_rank, world_size; // init world rank and size
     MPI_Init(&argc, &argv);
@@ -115,7 +78,7 @@ int main(int argc, char** argv){
     // calling MPI_P2P_Reduce
     long long int global_sum = 0; // only 0th rank process will have this value modified 
     uint64_t p2p_start_cycles = clock_now();
-    MPI_P2P_Reduce2(bigArr, &global_sum, arrSize, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    MPI_P2P_Reduce(bigArr, &global_sum, arrSize, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
     uint64_t p2p_end_cycles = clock_now();
 
     if (world_rank == 0){ // only root has the result and it prints it here
