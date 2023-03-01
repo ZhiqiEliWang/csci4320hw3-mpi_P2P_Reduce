2885799d664839e755398edd7f9307107bc34809
Zhiqi(Eli) Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-03-01 13:45:01 -0500

    Update comp_reduction.c

--- comp_reduction.c
+++ comp_reduction.c
@@ -36,21 +36,21 @@ int MPI_P2P_Reduce(long long int* send_data, // each process's partition of task
     MPI_Status send_status;
 
     while (stride < comm_size){
-        if ((self_rank / stride) % 2 == 0){ // even ranks after stride: receiver
+        if ((self_rank / stride) % 2 == 1){ // odd ranks after stride: sender
+            MPI_Isend(&local_sum, 1, MPI_LONG_LONG, self_rank-stride, 0, communicator, &send_req);
+            MPI_Wait(&send_req , &send_status);
+            printf("rank: %d sent\n", self_rank);
+        }
+        else{ // even ranks after stride: receiver
             long long int recv_buf;
             MPI_Irecv(&recv_buf, 1, MPI_LONG_LONG, self_rank+stride, MPI_ANY_TAG, communicator, &recv_req);
             MPI_Wait(&recv_req , &recv_status);
             printf("rank: %d received\n", self_rank);
             local_sum += recv_buf; // perform pairwise sum here
         }
-        else{ // odd ranks after stride: sender
-            MPI_Isend(&local_sum, 1, MPI_LONG_LONG, self_rank-stride, 0, communicator, &send_req);
-            MPI_Wait(&send_req , &send_status);
-            printf("rank: %d sent\n", self_rank);
-        }
-        stride *= 2;
         // printf("rank: %d about to hit MPI_Barrierr\n\n", self_rank);
         MPI_Barrier(communicator); // sync here
+        stride *= 2;
         if (self_rank == 0){printf("\n---------stride: %d done-----------\n", stride);}
         // printf("rank: %d just hit barrier\n\n", self_rank);
     }
